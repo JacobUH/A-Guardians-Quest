cf675333ac13d786d1d6ed0909014ea088828456
Rocky <ttruong4280@gmail.com>
2023-11-28 16:00:15 -0600

    Update CameraController.cs

--- Assets/Scripts/Misc/CameraController.cs
+++ Assets/Scripts/Misc/CameraController.cs
@@ -5,9 +5,14 @@ using UnityEngine.InputSystem;
 
 public class CameraController : SingletonMonobehaviour<CameraController>
 {
-    [SerializeField] float verticalSpeed = 1f;
-    [SerializeField] float horizontalSpeed = 20f;
+    [Range(0f,1f)] [SerializeField] float cameraSensitive = 0.5f;
+    [SerializeField] private float maxX = 180f;
+    [SerializeField] private float minX = 50f;
+    [SerializeField] private float maxY = 1f;
+    [SerializeField] private float minY = 0.3f; 
+    [Space]
     [SerializeField] float zoomSpeed = 3f;
+    [SerializeField] float mouseZoomSpeed = 10f;
     [SerializeField] float spanSpeed = 100f;
     [Space]
     [SerializeField] float topMaxHeight = 7f;
@@ -26,6 +31,7 @@ public class CameraController : SingletonMonobehaviour<CameraController>
 
     private CinemachineFreeLook freeLook;
     private Coroutine cameraSpanning;
+    private float currentSensitive;
 
     protected override void Awake()
     {
@@ -40,6 +46,22 @@ public class CameraController : SingletonMonobehaviour<CameraController>
         freeLook.m_XAxis.Value = GameObject.FindGameObjectWithTag("Player").transform.eulerAngles.y;
         freeLook.m_YAxis.Value = 0.65f;
         midRigTransposer = freeLook.GetRig(1).GetComponent<CinemachineFramingTransposer>();
+
+        freeLook.m_XAxis.m_MaxSpeed = (maxX - minX) * cameraSensitive + minX;
+        freeLook.m_YAxis.m_MaxSpeed = (maxY - minY) * cameraSensitive + minY;
+        currentSensitive = cameraSensitive;
+    }
+
+    private void Update()
+    {
+        if (currentSensitive != cameraSensitive)
+        {
+            freeLook.m_XAxis.m_MaxSpeed = (maxX - minX) * cameraSensitive + minX;
+            freeLook.m_YAxis.m_MaxSpeed = (maxY - minY) * cameraSensitive + minY;
+            currentSensitive = cameraSensitive;
+        }
+        if (Input.mouseScrollDelta.y > 0f) MouseZoomIn();
+        else if (Input.mouseScrollDelta.y < 0f) MouseZoomOut();
     }
 
     private IEnumerator SpanCameraCoroutine(float targetAngle)
@@ -73,8 +95,8 @@ public class CameraController : SingletonMonobehaviour<CameraController>
         Vector2 cameraMovement = InputReader.Instance.rightStickValue.normalized;
         if (cameraSpanning != null) return;
 
-        freeLook.m_YAxis.Value -= cameraMovement.y * Time.deltaTime * verticalSpeed;
-        freeLook.m_XAxis.Value += cameraMovement.x * Time.deltaTime * horizontalSpeed;
+        //freeLook.m_XAxis.Value += cameraMovement.x * Time.deltaTime * horizontalSpeed;
+        //freeLook.m_YAxis.Value -= cameraMovement.y * Time.deltaTime * verticalSpeed;
     }
 
     public void SpanCamera(float targetAngle)
@@ -108,6 +130,34 @@ public class CameraController : SingletonMonobehaviour<CameraController>
         if (freeLook.m_Orbits[2].m_Height > botMaxHeight) freeLook.m_Orbits[2].m_Height -= zoomSpeed * Time.deltaTime;
         else freeLook.m_Orbits[2].m_Height = botMaxHeight;
         if (freeLook.m_Orbits[2].m_Radius < botMaxRadius) freeLook.m_Orbits[2].m_Radius += zoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[2].m_Radius = botMaxRadius;
+    }
+
+    private void MouseZoomIn()
+    {
+        if (freeLook.m_Orbits[0].m_Height > topMinHeight) freeLook.m_Orbits[0].m_Height -= mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[0].m_Height = topMinHeight;
+        if (freeLook.m_Orbits[0].m_Radius > topMinRadius) freeLook.m_Orbits[0].m_Radius -= mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[0].m_Radius = topMinRadius;
+        if (freeLook.m_Orbits[1].m_Radius > midMinRadius) freeLook.m_Orbits[1].m_Radius -= mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[1].m_Radius = midMinRadius;
+        if (freeLook.m_Orbits[2].m_Height < botMinHeight) freeLook.m_Orbits[2].m_Height += mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[2].m_Height = botMinHeight;
+        if (freeLook.m_Orbits[2].m_Radius > botMinRadius) freeLook.m_Orbits[2].m_Radius -= mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[2].m_Radius = botMinRadius;
+    }
+
+    private void MouseZoomOut()
+    {
+        if (freeLook.m_Orbits[0].m_Height < topMaxHeight) freeLook.m_Orbits[0].m_Height += mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[0].m_Height = topMaxHeight;
+        if (freeLook.m_Orbits[0].m_Radius < topMaxRadius) freeLook.m_Orbits[0].m_Radius += mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[0].m_Radius = topMaxRadius;
+        if (freeLook.m_Orbits[1].m_Radius < midMaxRadius) freeLook.m_Orbits[1].m_Radius += mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[1].m_Radius = midMaxRadius;
+        if (freeLook.m_Orbits[2].m_Height > botMaxHeight) freeLook.m_Orbits[2].m_Height -= mouseZoomSpeed * Time.deltaTime;
+        else freeLook.m_Orbits[2].m_Height = botMaxHeight;
+        if (freeLook.m_Orbits[2].m_Radius < botMaxRadius) freeLook.m_Orbits[2].m_Radius += mouseZoomSpeed * Time.deltaTime;
         else freeLook.m_Orbits[2].m_Radius = botMaxRadius;
     }
 }
