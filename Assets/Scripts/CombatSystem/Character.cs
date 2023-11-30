1b4ce81a6a5916102668a0791c82ac43edf9da77
Rocky <ttruong4280@gmail.com>
2023-11-30 01:51:51 -0600

    Added some Npc. Balance Enemies HP. Added lights

--- Assets/Scripts/CombatSystem/Character.cs
+++ Assets/Scripts/CombatSystem/Character.cs
@@ -19,6 +19,8 @@ public class Character : MonoBehaviour, IDamageable
     [SerializeField] private TextMeshProUGUI healthBarTextMesh;
     [SerializeField] private CharacterBaseStats baseStats;
     [SerializeField] private CharacterEquipmentData equipmentData;
+    [SerializeField] protected GameObject healthUI;
+    [SerializeField] protected float healthDisplayDistance;
 
     private float currentHp;
     public float CurrentHpPercent => currentHp / GetMaxHp();
